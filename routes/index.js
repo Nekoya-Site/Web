ecc31aae11d0ffd6cc382b564a52a4058d73791d
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2021-11-09 09:53:46 +0700

    Added Router for About us, Register, Register-verification-*

--- routes/index.js
+++ routes/index.js
@@ -41,4 +41,19 @@ router.get('/payment', (_req, res) => {
     res.render('pages/payment')
 })
 
+router.get('/register', (_req, res) => {
+    res.render('pages/register')
+})
+
+router.get('/register-verification-completed', (_req, res) => {
+    res.render('pages/register-verification-completed')
+})
+
+router.get('/register-verification-sent', (_req, res) => {
+    res.render('pages/register-verification-sent')
+})
+router.get('/about-us', (_req, res) => {
+    res.render('pages/about-us')
+})
+
 module.exports = router;
