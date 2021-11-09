abd781bba8c2c2777e26befe388f5bae5d9697e6
Kelvin Samuel <kelvinbalkes@gmail.com>
2021-11-09 09:11:53 +0700

    Convert file html to ejs

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
+const session = require('express-session')
 const app = express()
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-const indexRouter = require('./routes/product');
+const indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express')
-
 const router = express.Router()
 
 router.get('/', (req, res) => {
+    res.render('pages/index');
+})
+
+router.get('/login', (req, res) => {
+    res.render('pages/login');
+})
+
+router.get('/forgot-password', (req, res) => {
+    res.render('pages/forgot-password');
+})
+
+router.get('/otp', (req, res) => {
+    res.render('pages/otp');
+})
+
+router.get('/product', (req, res) => {
     res.render('pages/product')
 })
 
