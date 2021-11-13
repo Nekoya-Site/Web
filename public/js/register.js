d20081696503fad5c04d065b4d67ae2706dff9de
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2021-11-14 04:59:00 +0700

    register js - Remove unused function

--- public/js/register.js
+++ public/js/register.js
@@ -6,12 +6,3 @@ function revealhidePassword() {
         x.type = "password";
     }
 }
-
-function revealhideConfirmationPassword() {
-    const x = document.getElementById("password_confirmation");
-    if (x.type === "password") {
-        x.type = "text";
-    } else {
-        x.type = "password";
-    }
-}
