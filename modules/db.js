5003dcd8b24ce7e07b40bd8ca8a62648a1c273e5
moepoi <moepoi@protonmail.com>
2021-11-15 17:35:31 +0700

    Revert some changes

--- modules/db.js
+++ modules/db.js
@@ -1,5 +1,10 @@
 var mysql = require('mysql2');
-const config = require('../conf');
+
+try {
+    const config = require('../config');
+} catch (e) {
+    console.log('No config file found');
+}
 
 function connect() {
     const connection = mysql.createConnection(config.db);
