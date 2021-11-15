113a1124698aa8654e29e5a233613751d688260e
moepoi <moepoi@protonmail.com>
2021-11-15 17:05:44 +0700

    Remove unused parameters

--- modules/db.js
+++ modules/db.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql2');
 const config = require('../config');
 
-function connect(sql, params) {
+function connect() {
     const connection = mysql.createConnection(config.db);
     return connection;
 }
