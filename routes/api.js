a0e0e6a667eaa9a45b7d082397182c769f08a2cb
moepoi <moepoi@protonmail.com>
2021-11-15 17:56:01 +0700

    Update api with current structure

--- routes/api.js
+++ routes/api.js
@@ -4,14 +4,14 @@ const db = require('../modules/db')
 
 router.get('/getproducts', (req, res) => {
     const conn = db.connect();
-    conn.execute('SELECT * FROM `product`', [], function(err, results) {
+    conn.execute('SELECT * FROM `products`', [], function(err, results) {
         res.json(results);
     });
 })
 
 router.get('/getproduct', (req, res) => {
     const conn = db.connect();
-    conn.execute('SELECT * FROM `product` WHERE `model` = ?', [req.query.id], function(err, results) {
+    conn.execute('SELECT * FROM `products` WHERE `ID` = ?', [req.query.id], function(err, results) {
         res.json(results);
     });
 })
