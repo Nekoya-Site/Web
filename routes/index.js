c8b9d4d663fc31d739979040b45ccc5ae74a32c8
moepoi <moepoi@protonmail.com>
2021-11-21 00:24:24 +0700

    Remove unused codes & files

--- routes/index.js
+++ routes/index.js
@@ -147,20 +147,12 @@ router.get("/product/:id", (req, res) => {
     });
 });
 
-router.get("/successadd", (_req, res) => {
-    res.render("pages/success-add");
-});
-
 router.get("/bag", (_req, res) => {
     res.render("pages/bag");
 });
 
 router.get("/checkout", (_req, res) => {
     res.render("pages/checkout");
-});
-
-router.get("/notifpay", (_req, res) => {
-    res.render("pages/notif-pay");
 });
 
 router.get("/payment", (_req, res) => {
