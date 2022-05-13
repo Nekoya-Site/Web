8d7b0d4628460d0b91d8996679fb1787d5187c9d
Moe <moe@chocola.dev>
2022-05-13 11:58:35 +0700

    Fix placement

--- index.js
+++ index.js
@@ -26,9 +26,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/api', api);
-
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
@@ -38,6 +35,9 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
+
+app.use('/', indexRouter);
+app.use('/api', api);
 
 app.listen(config.port, () => {
     console.log(`Server is running on port : ${config.port}`);
